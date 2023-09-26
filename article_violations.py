c74e1eb3ccd1ef1e93d8569654b14b060c823309
biancachiusano <bianca.cdchiusano@gmail.com>
2023-09-26 19:10:53 +0200

    Topic modelling and document clustering on the cases regarding dignity violating articles 2 or 3 or 8.

--- article_violations.py
+++ article_violations.py
@@ -12,35 +12,26 @@ violations_df = pd.DataFrame(columns=['File', 'Name', 'Two', 'Three', 'Eight', '
 def use_regular(clean):
     # removes all non-alphanumeric characters and spaces
     characters = re.sub(r'[^\w\s]', '', clean)
-    # removes all digits
-    #digits = re.sub(r'\d+', '', characters)
     # makes sure there is a space between uppercase and lowercase text
     final = re.sub(r'(?<=[a-z0-9])(?=[A-Z])', ' ', characters)
 
     return final
 
 
-def check_name(file):
-    return True
-
-
 def check_violation(reg):
     cut = reg.split()
 
-    #start_phrase = {"FOR, THESE, REASONS, THE, COURT"}
     start_phrase = {"REASONS"}
     start_index = None
     end_index = len(cut) - 1
-    #print("End Index ")
-    #print(end_index)
+
     for single in cut:
         if any(phrase in single for phrase in start_phrase):
             # find the start index
             start_index = cut.index(single) + len(single)
-            #print(start_index)
+
     if start_index is not None:
         ending = ' '.join(cut[start_index:end_index])
-        #print(ending)
 
         art_two_phrase = "Holds that there has been a violation of Article 2"
         art_three_phrase = "Holds that there has been a violation of Article 3"
@@ -79,9 +70,10 @@ def add_to_df(df, f, n, flag_two, flag_three, flag_eight, fact):
 
     # Append the new row to the DataFrame
     df.loc[len(df)] = new_row
-    #df = df.append(new_row, ignore_index=True)
+    # df = df.append(new_row, ignore_index=True)
 
     return df
+
 
 print(processed['Facts'][0])
 
@@ -95,7 +87,7 @@ for filename in dignity_cases['Case']:
             regular = use_regular(clean)
             keeping_count.append(filename)
             flag2, flag3, flag8 = check_violation(regular)
-            #print(flag2, flag3, flag8)
+            # print(flag2, flag3, flag8)
             if (flag2 is not False) or (flag3 is not False) or (flag8 is not False):
                 name = "TODO"
                 facts = processed['Facts'][count]
